e3e26f04705f957639f526a8c7bad469ed164aad
Bhushan Darandale <bhushan.darandale@gmail.com>
2017-07-11 19:21:04 +0530

    1.1
    1.1

--- src/collection/Map/HashMapExample.java
+++ src/collection/Map/HashMapExample.java
@@ -1,5 +1,43 @@
 package collection.Map;
 
-public class HashMapExample {
-
+import java.util.HashMap;
+import java.util.Map;
+ 
+public class HashMapExample
+{
+    public static void main(String[] args) 
+    {
+     
+        HashMap<String, Integer> map = new HashMap<>();
+         
+        print(map);
+        map.put("Bhushan", 10);
+        map.put("Gaurav", 30);
+        map.put("Suraj", 20);
+         
+        System.out.println("Size of map is:- " + map.size());   //Size of map is:- 3
+     
+        print(map);                                             //{Suraj=20, Bhushan=10, Gaurav=30}
+        if (map.containsKey("Bhushan")) 
+        {
+            Integer a = map.get("Bhushan");
+            System.out.println("value for key \"Bhushan\" is:- " + a);   //value for key "Bhushan" is:- 10
+        }
+         
+        map.clear();
+        print(map); 
+    }
+     
+    public static void print(Map<String, Integer> map) 
+    {
+        if (map.isEmpty()) 
+        {
+            System.out.println("map is empty");  //map is empty
+        }
+         
+        else
+        {
+            System.out.println(map);
+        }
+    }
 }
